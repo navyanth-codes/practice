b34901cba6c7cda04428122cc435ee553e4b4c7d
Navyanth reddy padala <118421920+navyanth-codes@users.noreply.github.com>
2023-08-06 05:50:15 +0530

    Add files via upload

---
+++ biitwise op.py
@@ -0,0 +1,28 @@
+# Given three positive integers a, b and c. Your task is to perform some bitwise operations on them as given below:
+# 1. d = a ^ a
+# 2. e = c ^ b
+# 3. f = a & b
+# 4. g = c | (a ^ a)
+# 5. e = ~ e
+# Note: ^ is for xor.
+# The output is d e f g
+
+def utility():
+    #The following 3 lines take input. Don't change them!
+    a=int(input())
+    b=int(input())
+    c=int(input())
+    
+    #Do a^a below
+    d=a^a
+    #Do c^b below
+    e=c^b
+    #Do a&b below
+    f=a&b
+    #Do c|(a^a) below
+    g=c|(a^a)
+    #Do ~e below
+    e=~e
+
+    #The line below prints the output. Don't change it!
+    print(d,e,f,g)
