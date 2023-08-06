afeb521c53fa82457881fec20e195a5599b644d1
Navyanth reddy padala <118421920+navyanth-codes@users.noreply.github.com>
2023-08-06 05:50:57 +0530

    Add files via upload

---
+++ precedence's.py
@@ -0,0 +1,20 @@
+# # Given four inputs that are stored in variables a,b,c, and d.
+# # You need to write an expression to evaluate the following formula.
+# # Use integer division. The expression should be a single statement.
+
+# ((a+b)//c)+d
+
+def utility():
+    #The following lines take the input. Don't change them!
+    a=int(input())
+    b=int(input())
+    c=int(input())
+    d=int(input())
+    
+    
+    #Complete the code below that stores the output of the expression
+    res = ((a+b)//c)+d
+    #Complete the code above that stores the output of the expression
+    
+    #This prints the output. Don't change it!
+    print(res)
