5045f409edff359711880655641884883e7502ee
Navyanth reddy padala <118421920+navyanth-codes@users.noreply.github.com>
2024-08-06 23:14:08 +0530

    Add files via upload

---
+++ stack.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class stack{
+    
+    public static void main(String[] args){
+        // Stack<Integer> stacks = new Stack<>();
+        // stacks.push(34);
+        // stacks.push(12);
+        // System.out.println(stacks.pop());
+        // System.out.println(stacks.pop());
+        
+        // Queue<Integer> queue = new LinkedList<>();
+
+        // queue.add(3);
+        // queue.add(4);
+
+        // System.out.println(queue.remove());
+
+        
+
+    }
+}
